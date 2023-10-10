8b7aaf0a64e275480082b8615037d94c42e01b00
Vini Fraga <vinifragam@gmail.com>
2023-10-10 15:33:17 -0300

    Fundamentos App Router - React Server Components

--- src/app/catalog/product/[...data]/page.tsx
+++ src/app/catalog/product/[...data]/page.tsx
@@ -1,17 +1,30 @@
+'use client'
+
 interface ProductProps {
   params: {
     data: string[]
   }
 }
 
+// Server Components => A gente não USA JavaScript no lado do cliente
+// Client Components => O JavaScript é enviado ao navegador (cliente)
+
 export default function Product({ params }: ProductProps) {
   const [productId, size, color] = params.data
+
+  console.log(params)
+
+  function addToCart() {
+    console.log('Adicionou ao carrinho')
+  }
 
   return (
     <div>
       <p>Product: {productId}</p>
       <p>Size: {size}</p>
       <p>Color: {color}</p>
+
+      <button onClick={addToCart}>Adicionar ao carrinho</button>
     </div>
   )
 }
