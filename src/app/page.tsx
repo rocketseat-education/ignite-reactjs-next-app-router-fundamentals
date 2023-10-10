9d6a7efa07b38e6e9ac3fe24d41ca4f5d404fd85
Vini Fraga <vinifragam@gmail.com>
2023-10-10 15:39:42 -0300

    Fundamentos App Router - Fetch de dados nos componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <h1>Hello World</h1>
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const response = await fetch('https://api.github.com/users/diego3g')
+  const user = await response.json()
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
